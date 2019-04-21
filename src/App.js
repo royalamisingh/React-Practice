4870c5de35519344b2604eabef7e5b01790b4b65
Amitabh Singh <s.amitabh1012@gmail.com>
2019-04-21 20:47:37 +0530

    First React Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import MemberRegistration from "./Members";
+import Contact from "./Contact";
+ 
+class Main extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/Members">Members</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/Members" component={Members}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
-
-export default App;
+ 
+export default Main;
